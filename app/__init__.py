211489f2999024134e2c59993f8709d8cd13f0e9
Pavitha P <pavithapari10@gmail.com>
2025-07-20 11:19:07 +0530

    Created a base.html and iinitialised the blueprint in the routes

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from app.config import Config
+from config import Config
 
 db=SQLAlchemy()
 bcrypt = Bcrypt()
