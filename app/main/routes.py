a21049f5cc7a88a12534419c9a3b00dc9d0f09b9
Pavitha P <pavithapari10@gmail.com>
2025-08-05 17:50:43 +0530

    Added PAgination

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,8 +10,9 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    posts=Post.query.order_by(Post.date_posted.desc()).all()
     current_year = datetime.now().year
+    page=request.args.get('page', 1, type=int) # Get the page number from the query string, default to 1
+    posts=Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=4) 
     return render_template('home.html', now=datetime.now(), year=current_year,user=current_user,posts=posts)
 @main.route('/details')
 def details():
