c9fbc1059893dc96aba834881ffb4755876cbde9
Pavitha P <pavithapari10@gmail.com>
2025-08-09 18:42:35 +0530

    Included some real stuff and also a ping route

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,6 +10,11 @@ from app.models import User,Like
 
 main = Blueprint('main', __name__)
 
+
+@main.route("/_ping")
+def ping():
+    return "OK", 200
+
 @main.route('/')
 def home():
     current_year = datetime.now().year
