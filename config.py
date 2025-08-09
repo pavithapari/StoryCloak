df463aa0c4fd73531090da4b3e3b1fe2bd640742
Pavitha P <pavithapari10@gmail.com>
2025-08-09 12:07:42 +0530

    Corrected the naming convention

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DB')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_ID')
-    MAIL_PASSWORD = os.environ.get('MAIL_PWD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
